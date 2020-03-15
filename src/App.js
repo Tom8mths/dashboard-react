369de72e0c954ebadc194dd58cec6774efb742be
Matheus Satelos <matheus.satelos@gmail.com>
2020-03-15 12:17:16 -0300

    feat: add sidebar and navbar to layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
-import Routes from "./routes/index";
 import "./App.scss";
+
+import Routes from "./routes/index";
+import Navbar from "./components/navbar";
+import SideBar from "./components/sidebar";
 
 export default function App() {
   return (
     <Router>
       <div>
+        <Navbar />
+        <SideBar />
         <Switch>
           <Routes />
         </Switch>
