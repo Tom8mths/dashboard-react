a59eabf29b9fe0f6924d75fc18cef8da7711e39f
Matheus Satelos <matheus.satelos@gmail.com>
2020-03-16 18:57:38 -0300

    feat: add events page

--- src/pages/events/events.jsx
+++ src/pages/events/events.jsx
@@ -1,15 +1,33 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Container, Wrapper, Section } from "./style";
 
 class Rooms extends Component {
-  state = {}
+  state = {};
 
-  render () {
+  render() {
     return (
-      <div className='App'>
-        <h1></h1>
-      </div>
-    )
+      <Container>
+        <Wrapper>
+          <h1>Events</h1>
+          <Section>
+            <form>
+              <label for="fname">First name:</label>
+              <input type="text" id="fname" name="fname" />
+              <label for="lname">Last name:</label>
+              <input type="text" id="lname" name="lname" />
+              <label for="lname">Last name:</label>
+              <input type="text" id="lname" name="lname" />
+              <textarea
+                id="subject"
+                name="subject"
+                placeholder="Write something.."
+              ></textarea>
+            </form>
+          </Section>
+        </Wrapper>
+      </Container>
+    );
   }
 }
 
-export default Rooms
+export default Rooms;
